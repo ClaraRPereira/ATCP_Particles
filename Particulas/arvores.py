fd51fae569991a582445cf17c8cfe08d93601038
pcosme <pedro.cosme.s@gmail.com>
2017-11-04 18:39:36 +0000

    arvores py com deviance

--- Particulas/arvores.py
+++ Particulas/arvores.py
@@ -12,7 +12,7 @@ import random,string,math,csv
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.ensemble import GradientBoostingClassifier as GBC
-#from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split
 import math
  
 # Load training data
@@ -47,9 +47,8 @@ W_valid = data_train[:,31][r>=0.9] # Extracting weights in training set
  
 # Train the GradientBoostingClassifier using our good features
 print 'Training classifier (this may take some time!)'
-gbc = GBC(n_estimators=50, max_depth=None,min_samples_leaf=200,max_features=30,verbose=1)
+gbc = GBC(n_estimators=100, max_depth=6,min_samples_leaf=200,max_features=30,verbose=1)
 gbc.fit(X_train,Y_train) 
-
 
 # #############################################################################
 # Plot feature importance
@@ -64,7 +63,51 @@ plt.xlabel('Relative Importance')
 plt.title('Variable Importance')
 plt.show()
 # ############################################################################# 
- 
+
+# #############################################################################
+# Plot training deviance
+
+# compute validation set deviance
+test_score = np.zeros((100,), dtype=np.float64)
+for i, y_pred in enumerate(gbc.staged_predict(X_valid)):
+    test_score[i] = gbc.loss_(Y_valid, y_pred)
+plt.title('Deviance')
+plt.plot(np.arange(100) + 1, gbc.train_score_, 'b-',
+         label='Training Set Deviance')
+plt.plot(np.arange(100) + 1, test_score, 'r-',
+         label='Validation Set Deviance')
+plt.legend(loc='upper right')
+plt.xlabel('Boosting Iterations')
+plt.ylabel('Deviance')
+plt.show()
+# #############################################################################
+
+
+# #############################################################################
+# Plot decision boundary
+
+#X_plot = data_train[:,0][r<0.9]
+#Y_plot = data_train[:,2][r<0.9]
+#plot_step = 1
+#x_min, x_max =  0, 400
+#y_min, y_max =  0, 400
+#x_min, x_max = X_plot.min() - 1, X_plot.max() + 1
+#y_min, y_max = Y_plot.min() - 1, Y_plot.max() + 1
+
+#xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),np.arange(y_min, y_max, plot_step))
+#plt.tight_layout(h_pad=0.5, w_pad=0.5, pad=2.5)
+#Z = gbc.predict_proba(X_valid)[:,1] 
+###Z = gbc.predict(np.c_[xx.ravel(), yy.ravel()])
+#Z = Z.reshape(xx.shape)
+#cs = plt.contourf(xx, yy, Z, cmap=plt.cm.RdYlBu)
+#plt.show()
+
+
+#colors = ['b','r']
+
+#plt.scatter(X_valid[:,0],X_valid[:,2], alpha=.1, s=4)
+#plt.show()
+# ############################################################################# 
  
 # Get the probability output from the trained method, using the 10% for testing
 prob_predict_train = gbc.predict_proba(X_train)[:,1]
@@ -74,7 +117,7 @@ prob_predict_valid = gbc.predict_proba(X_valid)[:,1]
 # This can be optimized though!
 pcut = np.percentile(prob_predict_train,85)
  
-# These are the final signal and background predictions
+# This are the final signal and background predictions
 Yhat_train = prob_predict_train > pcut 
 Yhat_valid = prob_predict_valid > pcut
  
@@ -134,69 +177,3 @@ for line in resultlist:
     theline = str(line[0])+','+str(line[1])+','+line[2]+'\n'
     fcsv.write(theline) 
 fcsv.close()
-<<<<<<< HEAD
-
-
-from matplotlib import pyplot as plt
- 
-Classifier_training_S = gbc.predict_proba(X_train[Y_train>0.5])[:,1].ravel()
-Classifier_training_B = gbc.predict_proba(X_train[Y_train<0.5])[:,1].ravel()
-Classifier_testing_A = gbc.predict_proba(X_test)[:,1].ravel()
-  
-c_max = max([Classifier_training_S.max(),Classifier_training_B.max(),Classifier_testing_A.max()])
-c_min = min([Classifier_training_S.min(),Classifier_training_B.min(),Classifier_testing_A.min()])
-  
-# Get histograms of the classifiers
-Histo_training_S = np.histogram(Classifier_training_S,bins=50,range=(c_min,c_max))
-Histo_training_B = np.histogram(Classifier_training_B,bins=50,range=(c_min,c_max))
-Histo_testing_A = np.histogram(Classifier_testing_A,bins=50,range=(c_min,c_max))
-  
-# Lets get the min/max of the Histograms
-AllHistos= [Histo_training_S,Histo_training_B]
-h_max = max([histo[0].max() for histo in AllHistos])*1.2
-# h_min = max([histo[0].min() for histo in AllHistos])
-h_min = 1.0
-  
-# Get the histogram properties (binning, widths, centers)
-bin_edges = Histo_training_S[1]
-bin_centers = ( bin_edges[:-1] + bin_edges[1:]  ) /2.
-bin_widths = (bin_edges[1:] - bin_edges[:-1])
-  
-# To make error bar plots for the data, take the Poisson uncertainty sqrt(N)
-ErrorBar_testing_A = np.sqrt(Histo_testing_A[0])
-# ErrorBar_testing_B = np.sqrt(Histo_testing_B[0])
-  
-# Draw objects
-ax1 = plt.subplot(111)
-  
-# Draw solid histograms for the training data
-ax1.bar(bin_centers-bin_widths/2.,Histo_training_B[0],facecolor='red',linewidth=0,width=bin_widths,label='B (Train)',alpha=0.5)
-ax1.bar(bin_centers-bin_widths/2.,Histo_training_S[0],bottom=Histo_training_B[0],facecolor='blue',linewidth=0,width=bin_widths,label='S (Train)',alpha=0.5)
- 
-ff = (1.0*(sum(Histo_training_S[0])+sum(Histo_training_B[0])))/(1.0*sum(Histo_testing_A[0]))
- 
-# # Draw error-bar histograms for the testing data
-ax1.errorbar(bin_centers, ff*Histo_testing_A[0], yerr=ff*ErrorBar_testing_A, xerr=None, ecolor='black',c='black',fmt='.',label='Test (reweighted)')
-# ax1.errorbar(bin_centers, Histo_testing_B[0], yerr=ErrorBar_testing_B, xerr=None, ecolor='red',c='red',fmt='o',label='B (Test)')
-  
-# Make a colorful backdrop to show the clasification regions in red and blue
-ax1.axvspan(pcut, c_max, color='blue',alpha=0.08)
-ax1.axvspan(c_min,pcut, color='red',alpha=0.08)
-  
-# Adjust the axis boundaries (just cosmetic)
-ax1.axis([c_min, c_max, h_min, h_max])
-  
-# Make labels and title
-plt.title("Higgs Kaggle Signal-Background Separation")
-plt.xlabel("Probability Output (Gradient Boosting)")
-plt.ylabel("Counts/Bin")
- 
-# Make legend with smalll font
-legend = ax1.legend(loc='upper center', shadow=True,ncol=2)
-for alabel in legend.get_texts():
-            alabel.set_fontsize('small')
-  
-# Save the result to png
-plt.savefig("Sklearn_gbc.png")
-=======
->>>>>>> fc7de953832db3ad1a8e8770bc3e98291ed651c7
