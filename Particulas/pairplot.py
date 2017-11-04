0fcbc72654652a2cc479164f70f4fc98376b58e7
Clara <clara.pereira@tecnico.ulisboa.pt>
2017-11-04 18:16:12 +0000

    pairplots

--- Particulas/pairplot.py
+++ Particulas/pairplot.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import pandas as pd
 import sklearn.cluster as cluster
 import time
+from pandas.plotting import scatter_matrix
 
 sns.set(style="ticks", color_codes=True)
 
@@ -33,7 +34,16 @@ sumWeights = np.sum(weights)
 sumSWeights = np.sum(weights[sSelector])
 sumBWeights = np.sum(weights[bSelector])
 
-names = ['DER_mass_MMC','DER_mass_transverse_met_lep','DER_mass_vis','DER_pt_h','DER_deltaeta_jet_jet','DER_mass_jet_jet','DER_prodeta_jet_jet','DER_deltar_tau_lep','DER_pt_tot','DER_sum_pt','DER_pt_ratio_lep_tau','DER_met_phi_centrality','DER_lep_eta_centrality','PRI_tau_pt','PRI_tau_eta','PRI_tau_phi,PRI_lep_pt','PRI_lep_eta','PRI_lep_phi','PRI_met','PRI_met_phi','PRI_met_sumet','PRI_jet_num','PRI_jet_leading_pt','PRI_jet_leading_eta','PRI_jet_leading_phi','PRI_jet_subleading_pt','PRI_jet_subleading_eta','PRI_jet_subleading_phi','PRI_jet_all_pt']
-dataset = pd.read_csv("training.csv", skiprows=1, names=names, delimiter=',')
+#names = ['EventID','DER_mass_MMC','DER_mass_transverse_met_lep','DER_mass_vis']#'DER_met_phi_centrality','DER_lep_eta_centrality','PRI_tau_pt','PRI_tau_eta','PRI_tau_phi','PRI_lep_pt','PRI_lep_eta','PRI_lep_phi','PRI_met','PRI_met_phi','PRI_met_sumet','PRI_jet_num','PRI_jet_leading_pt','PRI_jet_leading_eta','PRI_jet_leading_phi','PRI_jet_subleading_pt','PRI_jet_subleading_eta','PRI_jet_subleading_phi','PRI_jet_all_pt','weight','classification']
+#names = ['DER_pt_h','DER_deltaeta_jet_jet','DER_mass_jet_jet','DER_prodeta_jet_jet']
+#names = ['DER_deltar_tau_lep','DER_pt_tot','DER_sum_pt','DER_pt_ratio_lep_tau']
+names = ['DER_met_phi_centrality','DER_lep_eta_centrality','PRI_tau_pt','PRI_tau_eta']
+dataset = pd.read_csv("training.csv", skiprows=1, names=names, delimiter=',',usecols=['DER_met_phi_centrality','DER_lep_eta_centrality','PRI_tau_pt','PRI_tau_eta'])
+dataset = dataset[(dataset >= -900)]
+print(dataset.describe())
+#print(dataset.groupby('class').size())
 
 
+#dataset.hist()
+scatter_matrix(dataset)
+plt.show()
