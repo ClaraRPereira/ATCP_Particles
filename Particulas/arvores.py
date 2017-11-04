13e917c1cdd998416f51e5be1cfffd6ba09b5272
Clara <clara.pereira@tecnico.ulisboa.pt>
2017-11-04 00:44:31 +0000

    graficos correlaçoes

--- Particulas/arvores.py
+++ Particulas/arvores.py
@@ -12,7 +12,7 @@ import random,string,math,csv
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.ensemble import GradientBoostingClassifier as GBC
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
 import math
  
 # Load training data
@@ -40,7 +40,7 @@ W_valid = data_train[:,31][r>=0.9] # Extracting weights in training set
  
 # Train the GradientBoostingClassifier using our good features
 print 'Training classifier (this may take some time!)'
-gbc = GBC(n_estimators=50, max_depth=6,min_samples_leaf=200,max_features=30,verbose=1)
+gbc = GBC(n_estimators=50, max_depth=None,min_samples_leaf=200,max_features=30,verbose=1)
 gbc.fit(X_train,Y_train) 
  
 # Get the probability output from the trained method, using the 10% for testing
@@ -51,7 +51,7 @@ prob_predict_valid = gbc.predict_proba(X_valid)[:,1]
 # This can be optimized though!
 pcut = np.percentile(prob_predict_train,85)
  
-# This are the final signal and background predictions
+# These are the final signal and background predictions
 Yhat_train = prob_predict_train > pcut 
 Yhat_valid = prob_predict_valid > pcut
  
@@ -111,3 +111,66 @@ for line in resultlist:
     theline = str(line[0])+','+str(line[1])+','+line[2]+'\n'
     fcsv.write(theline) 
 fcsv.close()
+
+
+from matplotlib import pyplot as plt
+ 
+Classifier_training_S = gbc.predict_proba(X_train[Y_train>0.5])[:,1].ravel()
+Classifier_training_B = gbc.predict_proba(X_train[Y_train<0.5])[:,1].ravel()
+Classifier_testing_A = gbc.predict_proba(X_test)[:,1].ravel()
+  
+c_max = max([Classifier_training_S.max(),Classifier_training_B.max(),Classifier_testing_A.max()])
+c_min = min([Classifier_training_S.min(),Classifier_training_B.min(),Classifier_testing_A.min()])
+  
+# Get histograms of the classifiers
+Histo_training_S = np.histogram(Classifier_training_S,bins=50,range=(c_min,c_max))
+Histo_training_B = np.histogram(Classifier_training_B,bins=50,range=(c_min,c_max))
+Histo_testing_A = np.histogram(Classifier_testing_A,bins=50,range=(c_min,c_max))
+  
+# Lets get the min/max of the Histograms
+AllHistos= [Histo_training_S,Histo_training_B]
+h_max = max([histo[0].max() for histo in AllHistos])*1.2
+# h_min = max([histo[0].min() for histo in AllHistos])
+h_min = 1.0
+  
+# Get the histogram properties (binning, widths, centers)
+bin_edges = Histo_training_S[1]
+bin_centers = ( bin_edges[:-1] + bin_edges[1:]  ) /2.
+bin_widths = (bin_edges[1:] - bin_edges[:-1])
+  
+# To make error bar plots for the data, take the Poisson uncertainty sqrt(N)
+ErrorBar_testing_A = np.sqrt(Histo_testing_A[0])
+# ErrorBar_testing_B = np.sqrt(Histo_testing_B[0])
+  
+# Draw objects
+ax1 = plt.subplot(111)
+  
+# Draw solid histograms for the training data
+ax1.bar(bin_centers-bin_widths/2.,Histo_training_B[0],facecolor='red',linewidth=0,width=bin_widths,label='B (Train)',alpha=0.5)
+ax1.bar(bin_centers-bin_widths/2.,Histo_training_S[0],bottom=Histo_training_B[0],facecolor='blue',linewidth=0,width=bin_widths,label='S (Train)',alpha=0.5)
+ 
+ff = (1.0*(sum(Histo_training_S[0])+sum(Histo_training_B[0])))/(1.0*sum(Histo_testing_A[0]))
+ 
+# # Draw error-bar histograms for the testing data
+ax1.errorbar(bin_centers, ff*Histo_testing_A[0], yerr=ff*ErrorBar_testing_A, xerr=None, ecolor='black',c='black',fmt='.',label='Test (reweighted)')
+# ax1.errorbar(bin_centers, Histo_testing_B[0], yerr=ErrorBar_testing_B, xerr=None, ecolor='red',c='red',fmt='o',label='B (Test)')
+  
+# Make a colorful backdrop to show the clasification regions in red and blue
+ax1.axvspan(pcut, c_max, color='blue',alpha=0.08)
+ax1.axvspan(c_min,pcut, color='red',alpha=0.08)
+  
+# Adjust the axis boundaries (just cosmetic)
+ax1.axis([c_min, c_max, h_min, h_max])
+  
+# Make labels and title
+plt.title("Higgs Kaggle Signal-Background Separation")
+plt.xlabel("Probability Output (Gradient Boosting)")
+plt.ylabel("Counts/Bin")
+ 
+# Make legend with smalll font
+legend = ax1.legend(loc='upper center', shadow=True,ncol=2)
+for alabel in legend.get_texts():
+            alabel.set_fontsize('small')
+  
+# Save the result to png
+plt.savefig("Sklearn_gbc.png")
--- Particulas/histograms.py
+++ Particulas/histograms.py
@@ -2,6 +2,12 @@
 import random,string,math,csv
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import sklearn.cluster as cluster
+import time
+
+sns.set(style="ticks", color_codes=True)
 
 
 ### Reading an formatting training data
@@ -27,24 +33,61 @@ sumWeights = np.sum(weights)
 sumSWeights = np.sum(weights[sSelector])
 sumBWeights = np.sum(weights[bSelector])
 
-print numFeatures
-
+print (numFeatures)
+np.random.seed(19680801)
 # Plot Weights
 #for i in range(numFeatures
-i=4
+i=1
 plt.figure()
 Data = np.array([float(row[i]) for row in xs])
 sWeights = np.array(weights[sSelector])
 bWeights = np.array(weights[bSelector])
 bData = np.array(Data[bSelector])
 sData = np.array(Data[sSelector])
-bData = bData[(bData >= -900)]
-sData = sData[(sData >= -900)]
-plt.hist(bData,bins = "sqrt", normed = True, histtype = "step", label="Noise")
-plt.hist(sData,bins = "sqrt", normed = True, histtype = "step", label="H")
-plt.xlim(0,10)
-plt.title(str(all[0][i+1]))
-plt.legend()
-#plt.savefig(str(all[0][i+1]))
-plt.show()
+#bData = bData[(bData >= -900)]
+#sData = sData[(sData >= -900)]
 
+j=13
+Data2 = np.array([float(row[j]) for row in xs])
+sWeights2 = np.array(weights[sSelector])
+bWeights2 = np.array(weights[bSelector])
+bData2 = np.array(Data2[bSelector])
+sData2 = np.array(Data2[sSelector])
+#bData2 = bData2[(bData2 >= -900)]
+#sData2 = sData2[(sData2 >= -900)]
+#plt.hist(bData,bins = "sqrt", normed = True, histtype = "step", label="Noise")
+#plt.hist(sData,bins = "sqrt", normed = True, histtype = "step", label="H")
+#plt.xlim(0,10)
+plt.title(str(all[0][i+1]+all[0][j+1]) )
+
+#plt.savefig(str(all[0][i+1]))
+#plt.show()
+#sns.set_context('poster')
+#sns.set_color_codes()
+#plot_kwds = {'alpha' : 0.25, 's' : 80, 'linewidths':1}
+
+
+s = 2
+
+plt.scatter(bData, bData2, color='r', s=s, alpha=.4, label="Noise")
+plt.scatter(sData, sData2, color='b', s=s, alpha=.4, label="H")
+plt.legend(prop={'size': 9})
+plt.xlim(0, 300)
+plt.ylim(0, 300)
+plt.savefig(str(all[0][i+1]+all[0][j+1]))
+
+#plt.scatter(xs.T[0], xs.T[1], c=colours, **plot_kwds)
+
+#tips= np.array([map(float, row[1:3]) for row in all[1:]])
+#iris = pd.read_csv('training.csv')
+#tips=float(tips)
+#sns.set(style="ticks")
+#np.random.seed(sum(map(ord, "axis_grids")))
+
+
+#g = sns.FacetGrid(int(tips), col="sex", hue="smoker")
+#g.map(plt.scatter, "total_bill", "tip", alpha=.7)
+#g.add_legend();
+#iris = sns.load_dataset(tips2)
+#g = sns.pairplot(iris, hue="species")
+plt.show()
